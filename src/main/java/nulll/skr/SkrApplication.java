fab27968bc3b85c5417cbe18c41eafe02b91ee0b
sama god <sedaizhaotian@mail.scut.edu.cn>
2019-08-01 14:16:55 +0800

    commit with pages

---
+++ src/main/java/nulll/skr/SkrApplication.java
@@ -0,0 +1,13 @@
+package nulll.skr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkrApplication.class, args);
+    }
+
+}
