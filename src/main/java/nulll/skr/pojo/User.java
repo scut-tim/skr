43245932effdfad98c77472c24283dcd032c96c8
sama god <sedaizhaotian@mail.scut.edu.cn>
2019-08-02 13:02:40 +0800

    连接到远程数据库

---
+++ src/main/java/nulll/skr/pojo/User.java
@@ -0,0 +1,21 @@
+package nulll.skr.pojo;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/nulll/skr/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package nulll.skr.repository;
+
+
+import nulll.skr.pojo.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer > {
+}
