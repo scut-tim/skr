d5704a3263023522a611c01ec4970ded4afc55f9
sama god <sedaizhaotian@mail.scut.edu.cn>
2019-08-02 20:31:02 +0800

    注册

--- src/main/java/nulll/skr/controller/UserController.java
+++ src/main/java/nulll/skr/controller/UserController.java
@@ -5,6 +5,7 @@ import nulll.skr.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -13,7 +14,7 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-
+    @ResponseBody
     @PostMapping("/user")
     public void userRegister(User user){
 
