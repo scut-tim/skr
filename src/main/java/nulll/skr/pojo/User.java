6658915115601c2327c845ee779bad7edf9620b9
Rainfalllen <305207009@qq.com>
2019-08-04 13:46:29 +0800

    edit User's properties

--- src/main/java/nulll/skr/pojo/User.java
+++ src/main/java/nulll/skr/pojo/User.java
@@ -1,10 +1,11 @@
 package nulll.skr.pojo;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name="user")
-public class User {
+public class User implements Comparable<User>{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,10 +18,64 @@ public class User {
     @Column(name="email")
     private String email;
 
+    private Integer gender;
+    private Image headPortrait;
+    private String personalProfile;
+    private Integer attentionNum;
+    private Integer fansNum;
+    private Date birthday;
+
+    public Integer getAttentionNum() {
+        return attentionNum;
+    }
+    public void setAttentionNum(Integer attentionNum) {
+        if(attentionNum<0){
+            this.attentionNum=0;
+        }
+        this.attentionNum = attentionNum;
+    }
+
+    public Integer getFansNum() {
+        return fansNum;
+    }
+    public void setFansNum(Integer fansNum) {
+        if(fansNum<0){
+            this.fansNum=0;
+        }
+        this.fansNum = fansNum;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPersonalProfile() {
+        return personalProfile;
+    }
+    public void setPersonalProfile(String personalProfile) {
+        this.personalProfile = personalProfile;
+    }
+
+    public Image getHeadPortrait() {
+        return headPortrait;
+    }
+    public void setHeadPortrait(Image headPortrait) {
+        this.headPortrait = headPortrait;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
     public Integer getId() {
         return id;
     }
-
     public void setId(Integer id) {
         this.id = id;
     }
@@ -28,15 +83,12 @@ public class User {
     public String getUserName() {
         return userName;
     }
-
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
     public String getPassword() {
-        return password;
-    }
-
+        return password; }
     public void setPassword(String password) {
         this.password = password;
     }
@@ -47,5 +99,10 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    @Override
+    public int compareTo(User o){
+        return -1;
     }
 }
