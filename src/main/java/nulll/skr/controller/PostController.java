2c6cee8189c4eb7782676f011e56059cf1f7d93c
batiboy <40078009+batiboy@users.noreply.github.com>
2019-08-08 15:42:12 +0800

    帖子、用户、评论

--- src/main/java/nulll/skr/controller/PostController.java
+++ src/main/java/nulll/skr/controller/PostController.java
@@ -6,12 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 
@@ -19,10 +19,8 @@ import java.util.List;
 @RestController
 public class PostController {
 
-
     @Autowired
     private PostRepository postRepository;
-
 
     @PostMapping("/post")
     public boolean posting(Post post){
@@ -32,6 +30,32 @@ public class PostController {
         return true;
         ///////////////
 
+    }
+
+    @PostMapping("/image")
+    public Boolean upLoad(MultipartFile image){
+
+        //////
+        try {
+            byte[] imageByte = image.getBytes();
+
+            Post post = new Post();
+
+            post.setId(1);
+
+            post.setImage(imageByte);
+
+            postRepository.save(post);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+
+        return true;
+
+        ///////
     }
 
     @GetMapping("/posts/{pageNum}")
@@ -50,8 +74,20 @@ public class PostController {
 
     }
 
+    @PutMapping("/post")
+    public boolean updatePost(Post post){
+        postRepository.saveAndFlush(post);
+        return true;
+    }
 
-
-
-
+    @DeleteMapping("/post/{id}")
+    public boolean deletePost(@PathVariable(name = "id")int id){
+        Post post = postRepository.getOne(id);
+        if(post != null){
+            postRepository.delete(post);
+            return true;
+        }
+        else
+            return false;
+    }
 }
